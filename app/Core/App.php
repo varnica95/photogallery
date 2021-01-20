b988f29b7aa43533260d10b1c181950040809f06
Marin Varnica <marinvarnica@gmail.com>
2021-01-20 18:40:40 +0100

    added method to get item and fixed trait dir

--- app/Core/App.php
+++ app/Core/App.php
@@ -5,7 +5,7 @@ namespace App\Core;
 
 
 use App\Core\Routing\Router;
-use App\Traits\RouteAccessor;
+use App\Traits\App\RouteAccessor;
 
 class App
 {
@@ -34,5 +34,29 @@ class App
     public function getContainer()
     {
         return $this->container;
+    }
+
+    /**
+     * @param $name
+     * @return false|mixed
+     */
+    public function item($name)
+    {
+        return $this->getContainer()->$name;
+    }
+
+    public function run()
+    {
+        $router = $this->getContainer()->router;
+        $router->setPath($_SERVER['PATH_INFO'] ?? '/home');
+
+        $response = $router->response();
+
+        return $this->process($response);
+    }
+
+    protected function process($handler)
+    {
+        return $handler();
     }
 }
