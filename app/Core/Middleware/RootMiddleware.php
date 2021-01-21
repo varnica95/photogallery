e6e853bfbda4c0e890b3e0909a6ab038a314fab9
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 23:19:56 +0100

    added functionality for root middleware

--- app/Core/Middleware/RootMiddleware.php
+++ app/Core/Middleware/RootMiddleware.php
@@ -4,6 +4,8 @@
 namespace App\Core\Middleware;
 
 
+use App\Core\Http\Request;
+
 class RootMiddleware
 {
     /**
@@ -11,20 +13,34 @@ class RootMiddleware
      */
     protected $root;
 
+    /**
+     * RootMiddleware constructor.
+     */
     public function __construct()
     {
-        $this->root = function (){
-          dump('root');
+        $this->root = function (Request $request){
+            return $request;
         };
     }
 
-    public function add(Middleware $middleware)
+    /**
+     * @param Middleware $middleware
+     * @param $route
+     */
+    public function add(Middleware $middleware, $route)
     {
-
+        $next = $this->root;
+        $this->root = function (Request $request) use ($middleware, $next, $route){
+            return $middleware($request, $next, $route);
+        };
     }
 
-    public function handle()
+    /**
+     * @param Request $request
+     * @return false|mixed
+     */
+    public function handle(Request $request)
     {
-
+        return call_user_func($this->root, $request);
     }
 }
