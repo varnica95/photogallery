4b71c0797353a695880f8aeb1f2ac7ec99ba6d0e
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 18:27:28 +0100

    added TableMap

---
+++ app/Maps/TableMap.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Maps;
+
+
+use App\Models\User;
+
+class TableMap
+{
+    /**
+     * @var string[]
+     */
+    protected static $map = [
+        'users' => User::class
+    ];
+
+    /**
+     * @param $class
+     * @return string
+     */
+    public static function resolve($class)
+    {
+        foreach (self::$map as $key => $value) {
+            if ($class === $value){
+                return $key;
+            }
+        }
+    }
+
+    public static function getClass($table)
+    {
+        return self::$map[$table] ?? null;
+    }
+}
