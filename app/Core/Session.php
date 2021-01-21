15570018b3cf10688b4595be630b0a66380aadcf
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 21:39:39 +0100

    Session model

---
+++ app/Core/Session.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\Core;
+
+
+class Session
+{
+    /**
+     *
+     */
+    public static function start()
+    {
+        if (! isset($_SESSION)){
+            session_start();
+        }
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public static function get($name)
+    {
+        return self::has($name) ? $_SESSION[$name] : null;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public static function set($name, $value)
+    {
+        if (self::has($name)){
+            return;
+        }
+        $_SESSION[$name] = $value;
+    }
+
+    /**
+     *
+     */
+    public static function destroy()
+    {
+        if (isset($_SESSION)){
+            session_unset();
+
+            session_destroy();
+        }
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public static function has($name)
+    {
+        return isset($_SESSION[$name]);
+    }
+}
