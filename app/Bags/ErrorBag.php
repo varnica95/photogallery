19a0985640f4bf008b3dffb4b2ff26537d785bf1
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 11:27:02 +0100

    ErrorBag.php functionality

--- app/Bags/ErrorBag.php
+++ app/Bags/ErrorBag.php
@@ -6,5 +6,33 @@ namespace App\Bags;
 
 class ErrorBag
 {
+    /**
+     * @var
+     */
+    protected $errors;
 
+    /**
+     * @param $field
+     * @param $message
+     */
+    public function add($field, $message)
+    {
+        $this->errors[$field][] = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @return bool
+     */
+    public function has()
+    {
+        return ! empty($this->errors);
+    }
 }
