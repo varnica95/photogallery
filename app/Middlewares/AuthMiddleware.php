d4d418653c3d9dc4e4459c1dbea51b1d53d4e2e2
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 23:18:04 +0100

    Added AuthMiddleware functionality

--- app/Middlewares/AuthMiddleware.php
+++ app/Middlewares/AuthMiddleware.php
@@ -2,12 +2,18 @@
 
 namespace App\Middlewares;
 
+use App\Core\Http\Request;
 use App\Core\Middleware\Middleware;
 
 class AuthMiddleware implements Middleware
 {
-    public function __invoke()
+    public function __invoke(Request $request, callable $next, $route)
     {
-        // TODO: Implement __invoke() method.
+        if (empty($request->getSession('id')) && $_SERVER['PATH_INFO'] === $route) {
+            $request->setSession('id', 'dummy');
+            $request->redirect('login');
+        }
+
+        return $next($request);
     }
 }
