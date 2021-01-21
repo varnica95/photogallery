b8caccb3a103d49082127df6d7a1adb68f864432
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 21:02:07 +0100

    LoginController added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\Core\Controller;
+use App\Core\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function index(Request $request)
+    {
+       $this->view('login.index');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+           'username' => 'required|name',
+           'password' => 'required|min:5',
+        ]);
+
+        $user = User::login($data);
+    }
+}
