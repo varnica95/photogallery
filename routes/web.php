ab4aed523e800279a883e080c99e458df88ba252
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 21:01:36 +0100

    I've added a login route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
 $app->get('/home', [HomeController::class, 'index']);
 
 $app->post('/register', [RegisterController::class, 'store']);
 $app->get('/register', [RegisterController::class, 'index']);
+
+$app->post('/login', [LoginController::class, 'store']);
+$app->get('/login', [LoginController::class, 'index']);
