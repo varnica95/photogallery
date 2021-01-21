2bf9df0040200810f8a29e924671523d533d621c
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 11:18:15 +0100

    Added Validator functionality

--- app/Core/Validation/Validator.php
+++ app/Core/Validation/Validator.php
@@ -35,6 +35,28 @@ class Validator
 
     public function validate()
     {
-        
+        foreach ($this->rules as $field => $rules) {
+            foreach ($rules as $rule) {
+                $this->validateRule($field, $rule);
+            }
+        }
+    }
+
+    private function validateRule($field, Rule $rule)
+    {
+        if (! $rule->passes($field, $this->getValueFrom($this->data, $field), $this->data))
+        {
+            dump($rule->message($field));
+        }
+    }
+
+    /**
+     * @param $data
+     * @param $field
+     * @return mixed|null
+     */
+    private function getValueFrom($data, $field)
+    {
+        return $data[$field] ?? null;
     }
 }
