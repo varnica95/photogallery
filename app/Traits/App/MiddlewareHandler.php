e342d7d740521a1cb3e923308c67b1a4573ed1cb
Marin Varnica <marinvarnica@gmail.com>
2021-01-21 23:16:10 +0100

    Added MiddlewareHandler trait

---
+++ app/Traits/App/MiddlewareHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Traits\App;
+
+
+use App\Core\Config;
+
+trait MiddlewareHandler
+{
+    public function middleware($middleware)
+    {
+        $route = $this->item('router')->getLastInsertedRoute();
+
+        if (is_object($middleware)){
+            $this->item('middleware')->add($middleware, $route);
+        }
+
+        if (! $this->exists($middleware = $this->getMiddleware($middleware))){
+            return null;
+        }
+        return $this->item('middleware')->add(new $middleware, $route);
+    }
+
+    protected function getMiddleware($middleware)
+    {
+        return Config::env('middleware.namespace') . ucfirst($middleware) . 'Middleware';
+    }
+
+    protected function exists($middleware)
+    {
+        return class_exists($middleware);
+    }
+}
