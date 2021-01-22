c77e71128ff763a8ababbcec08323ab571b27c4d
Marin Varnica <marinvarnica@gmail.com>
2021-01-22 12:44:25 +0100

    added method for signing out

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,11 +10,17 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
+    /**
+     * @param Request $request
+     */
     public function index(Request $request)
     {
        $this->view('login.index');
     }
 
+    /**
+     * @param Request $request
+     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -42,5 +48,18 @@ class LoginController extends Controller
 
         $request->setSession('id', $user->id);
         $request->redirect('home');
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function out(Request $request)
+    {
+        if ($request->user()){
+            $request->destroySession();
+            $request->redirect('home');
+        }
+
+        $request->redirect('login');
     }
 }
