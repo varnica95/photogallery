608f1742432b37bdb5ceb63b8d9e60befb59a8ad
Marin Varnica <marinvarnica@gmail.com>
2021-01-23 19:46:56 +0100

    added env

---
+++ env.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'name' => 'gallery',
+        'username' => 'root',
+        'password' => 'secret',
+    ],
+    'views' => [
+        'root' => __DIR__ . '/resources/views/layouts/app.phtml',
+        'directory' => __DIR__ . '/resources/views/',
+        'extension' => '.phtml'
+    ],
+    'middleware' => [
+        'namespace' => 'App\\Middlewares\\'
+    ],
+    'storage' => [
+        'galleries' => '/storage/galleries/'
+    ]
+];
