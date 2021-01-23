8b898276e6df70e054220c421c65ecb2950dae29
Marin Varnica <marinvarnica@gmail.com>
2021-01-23 13:26:13 +0100

    Added mrthod for getting a default image

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -4,9 +4,16 @@
 namespace App\Models;
 
 
+use App\Core\Includes\Config;
 use App\Core\Model;
 
 class Gallery extends Model
 {
-
+    /**
+     * @return string
+     */
+    public function defaultImage()
+    {
+        return Config::env('storage.galleries') . 'gallery_image.png';
+    }
 }
