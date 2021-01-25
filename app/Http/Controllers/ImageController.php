a0487a3abf5164b07f5a8680f6930b5ae6dc8e63
Marin Varnica <marinvarnica@gmail.com>
2021-01-25 22:52:06 +0100

    added store method for storing gallery photos

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers;
 
 use App\Core\Controller;
 use App\Core\Http\Request;
+use App\Models\Gallery;
+use App\Models\Image;
 
 class ImageController extends Controller
 {
@@ -22,10 +24,43 @@ class ImageController extends Controller
 
     public function store(Request $request)
     {
+        $noun = array_keys($request->all())[2];
+
         $data = $request->validate([
             'title' => 'required',
-            'images.*.type' => 'image',
-            'gallery_id' => 'required'
+            'gallery_id' => 'required',
+            $noun . '.*.type' => 'required|image',
+        ]);
+
+        $gallery = Gallery::find($request->gallery_id);
+
+        $images = [];
+        //check if the $noun is assoc or not
+        if (array_keys($request->$noun) !== range(0, count($request->$noun) - 1)) {
+            $images = Image::create([
+                'title' => $request->title,
+                'gallery_id' => $request->gallery_id,
+            ]);
+
+            $images->image = $request->$noun;
+            $images->uploadImage();
+            $images->save();
+        }else{
+            foreach ($request->$noun as $key => $value) {
+                $images[$key] = Image::create([
+                    'title' => $request->title,
+                    'gallery_id' => $request->gallery_id,
+                ]);
+
+                $images[$key]->image = $request->$noun[$key];
+                $images[$key]->uploadImage();
+                $images[$key]->save();
+            }
+        }
+
+        $this->view('images.upload', [
+            'user' => $request->user(),
+            'success' => 'Gallery successfully created.'
         ]);
     }
 }
